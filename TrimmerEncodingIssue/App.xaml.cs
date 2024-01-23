d7e2917af67fb9e5dc7ed9cefb35d2d0d5407cc2
= <rozycki.woj@gmail.com>
2024-01-23 13:38:24 +0100

    sample project

---
+++ TrimmerEncodingIssue/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TrimmerEncodingIssue
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TrimmerEncodingIssue/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace TrimmerEncodingIssue
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            //Without trimming asciiEnc = US-ASCII
+            //After trimming asciiEnc = Globalization_cp_20127
+            var asciiEnc = System.Text.Encoding.ASCII.EncodingName;
+
+            CounterBtn.Text = $"{count}. {asciiEnc}";
+            SemanticScreenReader.Announce(CounterBtn.Text);
+
+            //GetEncoding will throw exception when app is trimmed
+            var enc = Encoding.GetEncoding(Encoding.ASCII.EncodingName, EncoderFallback.ExceptionFallback, DecoderFallback.ExceptionFallback);
+            Console.WriteLine($"Encoding: {enc?.EncodingName}");
+        }
+    }
+
+}
